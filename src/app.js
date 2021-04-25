84eaf7cc5c3bbbc7d39d762b85db2732fa42881e
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-04-25 14:03:26 +0200

    Partials and refactoring

--- src/app.js
+++ src/app.js
@@ -1,38 +1,43 @@
+const MY_NAME = 'Leandro Rodrigo'
+
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for express config
 const publicPath = path.join(__dirname, '../public/')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
-        name: 'Leandro Rodrigo'
+        name: MY_NAME,
+        title: 'Weather'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About',
-        name: 'Leandro Rodrigo'
+        name: MY_NAME,
+        title: 'About'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help',
-        name: 'Leandro Rodrigo',
-        message: 'Quis mollitia esse maxime doloremque accusamus qui sed.Reiciendis temporibus molestiae cupiditate sit ea recusandae velit.Corrupti non laboriosam illo.Repellendus unde saepe dolor sit rerum et ducimus praesentium'
+        message: 'Quis mollitia esse maxime doloremque accusamus qui sed.Reiciendis temporibus molestiae cupiditate sit ea recusandae velit.Corrupti non laboriosam illo.Repellendus unde saepe dolor sit rerum et ducimus praesentium',
+        name: MY_NAME,
+        title: 'Help'
     })
 })
 
@@ -40,8 +45,8 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     res.send({
         forecast: `It's 11 degrees outside in New York City, New York, and there are chances to rain because there's 90% of humidity, and it feels like 8 degrees`,
-
-        locatoion: 'NYC'
+        locatoion: 'NYC',
+        name: MY_NAME
     })
 })
 
