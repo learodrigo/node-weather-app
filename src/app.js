3382cf1b4238836154e6d73fd781455424fa2e7a
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-04-25 03:51:08 +0200

    Serving static content

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+// Home
+const publicPath = path.join(__dirname, '../pulic')
+app.use(express.static(publicPath))
+
+// Forecast weather message
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: `It's 11 degrees outside in New York City, New York, and there are chances to rain because there's 90% of humidity, and it feels like 8 degrees`,
+
+        locatoion: 'NYC'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
